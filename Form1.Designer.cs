67dd30c820d7b018b236ee2cc3603a39b987454e
KIM Taegyoon <steloflute@gmail.com>
2023-10-04 15:01:28 +0900

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HalfFull {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxHalf = new System.Windows.Forms.TextBox();
+            this.textBoxFull = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(12, 24);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(360, 21);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Halfwidth:";
+            // 
+            // textBoxHalf
+            // 
+            this.textBoxHalf.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxHalf.Location = new System.Drawing.Point(12, 63);
+            this.textBoxHalf.Name = "textBoxHalf";
+            this.textBoxHalf.ReadOnly = true;
+            this.textBoxHalf.Size = new System.Drawing.Size(360, 21);
+            this.textBoxHalf.TabIndex = 3;
+            // 
+            // textBoxFull
+            // 
+            this.textBoxFull.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFull.Location = new System.Drawing.Point(12, 102);
+            this.textBoxFull.Name = "textBoxFull";
+            this.textBoxFull.ReadOnly = true;
+            this.textBoxFull.Size = new System.Drawing.Size(360, 21);
+            this.textBoxFull.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fullwidth:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 138);
+            this.Controls.Add(this.textBoxFull);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHalf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "HalfFull (C) 2014 KIM Taegyoon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxHalf;
+        private System.Windows.Forms.TextBox textBoxFull;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HalfFull {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e) {
+            textBoxHalf.Text = Microsoft.VisualBasic.Strings.StrConv(textBoxInput.Text, Microsoft.VisualBasic.VbStrConv.Narrow);
+            textBoxFull.Text = Microsoft.VisualBasic.Strings.StrConv(textBoxInput.Text, Microsoft.VisualBasic.VbStrConv.Wide);
+        }
+    }
+}
